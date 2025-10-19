efee0ed06a59e09904586f571e533a8db22a5977
reterrr <sharonovnikola1@gmail.com>
2025-10-19 19:09:05 +0200

    fix: fixing nullptr dereference
    ExprPtr init_ can be nullptr; Check added

---
+++ ast/nodes/expr/init_declarator_expr.hpp
@@ -0,0 +1,29 @@
+//
+// Created by yhwach on 9/8/25.
+//
+
+#ifndef INIT_DECLARATOR_EXPR_HPP
+#define INIT_DECLARATOR_EXPR_HPP
+
+#include "expr.hpp"
+#include "../visit/expr_visitor.hpp"
+
+namespace ast {
+    struct InitDeclarator final : Expr {
+        lex::SymId name;
+        ExprPtr init_;
+
+        InitDeclarator(const lex::SymId name, ExprPtr init, const lex::Loc &loc)
+            : Expr(NodeKind::Expr_DeclInit, loc), name(name), init_(init) {
+            if (init_) init_->parent = this;
+        }
+
+        void accept(visitor::ExprVisitor &) override;
+    };
+
+    inline void InitDeclarator::accept(visitor::ExprVisitor &v) {
+        v.visit(*this);
+    }
+}
+
+#endif //INIT_DECLARATOR_EXPR_HPP
